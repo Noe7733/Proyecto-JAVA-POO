6bde07e201e4e86c3049a9afd8345aa661ad9f54
Jose Eduardo Orozco Cardenas <eduardo.orozco66@unach.mx>
2021-04-08 23:51:09 -0500

    3er cambio

--- src/Ferreteria/principal.java
+++ src/Ferreteria/principal.java
@@ -17,6 +17,7 @@ public class principal {
      */
     public static void main(String[] args) {
         System.out.println("Hola Mundo");
+        System.out.println("Este es nuestro proyecto");
         // TODO code application logic here
     }
     
