3f71aea20802c37375aeed466d2e2e95dd11cdf8
Jose Eduardo Orozco Cardenas <eduardo.orozco66@unach.mx>
2021-04-08 21:41:18 -0500

    2do cambio

--- src/Ferreteria/principal.java
+++ src/Ferreteria/principal.java
@@ -16,6 +16,7 @@ public class principal {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        System.out.println("Hola Mundo");
         // TODO code application logic here
     }
     
